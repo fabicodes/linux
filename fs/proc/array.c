8c866c652a0f5f83617dd472dde5f0640a0a4224
Fabian Jackl <fabian@jackl.org>
2020-01-27 13:34:26 +0100

    Hello World in /proc/PID/pm8

--- fs/proc/array.c
+++ fs/proc/array.c
@@ -427,6 +427,13 @@ int proc_pid_status(struct seq_file *m, struct pid_namespace *ns,
 	return 0;
 }
 
+int proc_pid_pm8(struct seq_file *m, struct pid_namespace *ns,
+			struct pid *pid, struct task_struct *task)
+{
+	seq_puts(m, "Hello World");
+	return 0;
+}
+
 static int do_task_stat(struct seq_file *m, struct pid_namespace *ns,
 			struct pid *pid, struct task_struct *task, int whole)
 {
--- fs/proc/internal.h
+++ fs/proc/internal.h
@@ -146,6 +146,8 @@ extern int proc_tgid_stat(struct seq_file *, struct pid_namespace *,
 			  struct pid *, struct task_struct *);
 extern int proc_pid_status(struct seq_file *, struct pid_namespace *,
 			   struct pid *, struct task_struct *);
+extern int proc_pid_pm8(struct seq_file *, struct pid_namespace *,
+			   struct pid *, struct task_struct *);
 extern int proc_pid_statm(struct seq_file *, struct pid_namespace *,
 			  struct pid *, struct task_struct *);
 
